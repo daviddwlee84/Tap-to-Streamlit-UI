9f8d795c937188cf6686d8b51354c12caa81c6f4
Da-Wei Lee <daviddwlee84@gmail.com>
2024-07-26 10:43:34 +0800

    Add required warning for arguments without default value

--- ui.py
+++ ui.py
@@ -4,14 +4,25 @@ from cli import MyTap
 
 st.title("Streamlit Tap Converter")
 
+with st.expander("Pydantic Model"):
+    # Create and display the Pydantic model based on the Tap class
+    PydanticModel = create_pydantic_model(MyTap)
+    st.write("Pydantic Model Schema")
+    st.json(PydanticModel.model_json_schema())
+
+allow_empty = st.checkbox("Allow Empty")
+
 # Create the Streamlit UI based on the Tap class
-inputs = create_streamlit_ui(MyTap)
+inputs, empty_args = create_streamlit_ui(MyTap, required_warning=not allow_empty)
 
 # Display the input dictionary
 st.write("Input Values")
 st.write(inputs)
 
-# Create and display the Pydantic model based on the Tap class
-PydanticModel = create_pydantic_model(MyTap)
-st.write("Pydantic Model Schema")
-st.json(PydanticModel.model_json_schema())
+if not allow_empty and empty_args:
+    st.warning(
+        f"Missing {len(empty_args)} required arguments: {empty_args}. Fill to continue."
+    )
+
+if st.button("Continue", disabled=bool(empty_args) and not allow_empty):
+    st.text("You continued!")
--- utils.py
+++ utils.py
@@ -13,56 +13,87 @@ from pydantic import create_model, BaseModel
 from tap import Tap
 
 
-def _get_streamlit_input(name: str, arg_type: Any, default: Any):
+def _get_streamlit_input(
+    name: str, arg_type: Type, default: Any, is_required: bool = False
+):
     # Handle Literal types (which are Enums or fixed choices)
     if get_origin(arg_type) is Literal:
         choices = get_args(arg_type)
         if isinstance(choices[0], str):
-            return st.selectbox(name, choices, index=choices.index(default))
+            return st.selectbox(
+                name,
+                choices,
+                index=choices.index(default),
+                help="Required" if is_required else None,
+            )
         elif isinstance(choices[0], bool):
-            return st.checkbox(name, value=default)
+            return st.checkbox(
+                name, value=default, help="Required" if is_required else None
+            )
 
     # Handle Optional types
     elif get_origin(arg_type) is Union and type(None) in get_args(arg_type):
         inner_type = [t for t in get_args(arg_type) if t is not type(None)][0]
-        value = _get_streamlit_input(name, inner_type, default)
+        value = _get_streamlit_input(name, inner_type, default, is_required)
         return value if value != "" else None
 
     # Handle simple types
     if arg_type is str:
-        return st.text_input(name, value=default)
+        return st.text_input(
+            name, value=default, help="Required" if is_required else None
+        )
     elif arg_type is bool:
-        return st.checkbox(name, value=default)
+        return st.checkbox(
+            name, value=default, help="Required" if is_required else None
+        )
     elif arg_type is int:
-        return st.number_input(name, value=default, step=1)
+        return st.number_input(
+            name, value=default, step=1, help="Required" if is_required else None
+        )
     elif arg_type is float:
-        return st.number_input(name, value=default)
+        return st.number_input(
+            name, value=default, help="Required" if is_required else None
+        )
 
     return None
 
 
 def _parse_tap(tap_class: Tap) -> Dict[str, Tuple[Type, Any]]:
     results = {}
-    obj = tap_class()
-    class_dict = obj._get_class_dict()
+    obj: Tap = tap_class()
+    default_value_dict = obj._get_class_dict()
     for name, arg_type in obj._annotations.items():
-        default = class_dict.get(name)
-        results[name] = (arg_type, default)
+        default = default_value_dict.get(name)
+        is_required = name not in default_value_dict
+        results[name] = (arg_type, default, is_required)
     return results
 
 
-def create_streamlit_ui(tap_class: Tap) -> Dict[str, Any]:
+def _streamlit_is_empty(arg_type: Type, value: Any) -> bool:
+    if arg_type is str:
+        return value is None or not value
+    return value is None
+
+
+def create_streamlit_ui(
+    tap_class: Tap, required_warning: bool = True
+) -> Dict[str, Any]:
     inputs = {}
+    empty_args = []
 
-    for name, (arg_type, default) in _parse_tap(tap_class).items():
+    for name, (arg_type, default, is_required) in _parse_tap(tap_class).items():
         inputs[name] = _get_streamlit_input(name, arg_type, default)
+        if is_required and _streamlit_is_empty(arg_type, inputs[name]):
+            empty_args.append(name)
+            if required_warning:
+                st.warning(f"Field {name} is required but empty.")
 
-    return inputs
+    return inputs, empty_args
 
 
 def create_pydantic_model(tap_class: Tap) -> BaseModel:
     fields = {}
-    for name, (arg_type, default) in _parse_tap(tap_class).items():
+    for name, (arg_type, default, _) in _parse_tap(tap_class).items():
         fields[name] = (arg_type, default)
 
     # Dynamically create Pydantic model
